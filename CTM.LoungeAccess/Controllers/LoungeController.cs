eb179ba77b63df3e79ac6b8a89b258316e72e627
Dean Ashton <dean.ashton@travelctm.com>
2019-07-09 09:52:27 +1000

    initial checkin

---
+++ CTM.LoungeAccess/Controllers/LoungeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CTM.LoungeAccess.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoungeController : ControllerBase
+    {
+        // GET: api/Lounge
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Lounge/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Lounge
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Lounge/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ CTM.LoungeAccess/Models/Lounge.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CTM.LoungeAccess.Models
+{
+    public class Lounge
+    {
+        //public int Id { get; set; }
+        //public string Title { get; set; }
+        //public string Description { get; set; }
+        //public IEnumerable<AmenityEnum> Amenities { get; set; }
+        //public int Id { get; set; }
+        //public int Id { get; set; }
+        //public int Id { get; set; }
+        //public int Id { get; set; }
+        //public int Id { get; set; }
+
+    }
+}
