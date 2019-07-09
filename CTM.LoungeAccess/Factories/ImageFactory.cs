352c12270c4c2f8764c3143f76ec65aec47e6808
Robert Talbot <robtalbotis@gmail.com>
2019-07-09 14:47:45 +1000

    made a method to get image url from a google place photo reference code

--- CTM.LoungeAccess/Factories/ImageFactory.cs
+++ CTM.LoungeAccess/Factories/ImageFactory.cs
@@ -9,5 +9,15 @@ namespace CTM.LoungeAccess.Factories
     {
         //https://maps.googleapis.com/maps/api/place/photo?maxwidth=600&photoreference=CmRaAAAARdSJPgF_YmqfksONDOy3GJia-7UfVj5TM0652dxp_fa5XrNSOhCQqTvbqVyhz9g8-kpnGKWikYvhVHKkZ_Fx0BuxonG1n3k5XWvHbNoiqpEGpcHIZCkygYLc4zJg2R8OEhA6IBNbjgcqt3FSDS1lnWpJGhTED9VyavNNYJmpjHbGQwG6bZgJ-g&key=AIzaSyAa-Cb3X-JCgyiUWJ_W3_npAc-T5tI0xjg
 
+        public static string GetNiceImageUrl(string photoReference)
+        {
+            var imageUrl = "";
+            var gMap = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=600&photoreference=";
+            var gMapKey = "AIzaSyAa-Cb3X-JCgyiUWJ_W3_npAc-T5tI0xjg";
+
+            imageUrl = string.Format("{0}{1}&key={2}", gMap, photoReference, gMapKey);
+            return imageUrl;
+        }
+
     }
 }
