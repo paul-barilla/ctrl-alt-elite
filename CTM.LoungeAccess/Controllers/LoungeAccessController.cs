82acea50591045c779694708a2cd7a1c07ccdccf
Dean Ashton <dean.ashton@travelctm.com>
2019-07-09 11:22:06 +1000

    LoungeAccess controller

---
+++ CTM.LoungeAccess/Controllers/LoungeAccessController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.DrawingCore;
+using System.Linq;
+using System.Threading.Tasks;
+using CTM.LoungeAccess.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QRCoder;
+using ZXing;
+
+namespace CTM.LoungeAccess.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoungeAccessController : ControllerBase
+    {
+        // GET: api/LoungeAccess/5
+        [HttpGet("{id}")]
+        public ActionResult Get(int loungeId)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode("SkyLounge Sydney Airport T1", QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+
+            var la = new LoungeAccessCode() { LoungeId = 1, Title = "SkyLounge", QRCodeImage = qrCodeImage };
+            return Ok(la);
+        }
+
+    }
+}
---
+++ CTM.LoungeAccess/Models/LoungeAccess.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DrawingCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CTM.LoungeAccess.Models
+{
+    public class LoungeAccessCode
+    {
+        public int LoungeId { get; set; }
+        public string Title { get; set; }
+        public Bitmap QRCodeImage { get; set; }
+    }
+}
