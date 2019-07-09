335c44c2970669d3badfe0f260e23af4f446561b
Robert Talbot <robtalbotis@gmail.com>
2019-07-10 08:55:54 +1000

    added new search controller and view page to look like mobile design

---
+++ CTM.LoungeAccess/Controllers/SearchController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using CTM.LoungeAccess.Models;
+using CTM.LoungeAccess.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CTM.LoungeAccess.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly ILoungeSearchService _loungeSearchService;
+        public SearchController(IMapper mapper)
+        {
+            _loungeSearchService = new LoungeSearchService(mapper);
+        }
+
+        public async Task<IActionResult> Index(string airportcode)
+        {
+            SearchRequest searchRequest = new SearchRequest() { AirportCode = string.IsNullOrEmpty(airportcode) ? "SYD" : airportcode };
+
+            ViewBag.AirportCode = string.IsNullOrEmpty(airportcode) ? "SYD" : airportcode;
+
+            var something = await _loungeSearchService.GetSearchResultsFromGoogleAsync(searchRequest);
+
+            List<Lounge> list = new List<Lounge>();
+            list = something.ToList();
+
+            return View(list);
+        }
+    }
+}
